6ba0a6862b79b518ac1ad5290167f23dd0cfe4d5
Naga <challanaganarasimhareddy@gmail.com>
2026-01-30 17:27:47 +0530

    chaining - basic

---
+++ 4_chains/1_basic_chaining.py
@@ -0,0 +1,23 @@
+from langchain_core.runnables import RunnableLambda
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from common.model import get_model
+
+model = get_model()
+
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {num_jokes} jokes"),
+]
+
+prompt_template = ChatPromptTemplate.from_messages(messages)
+
+# Custom lambda function to uppercase the output
+uppercase_output = RunnableLambda(lambda x: x.upper())
+count_words = RunnableLambda(lambda x: f"Word Count: {len(x.split())}\n{x}")
+
+chain = prompt_template | model | StrOutputParser() | uppercase_output | count_words
+
+result = chain.invoke({"topic": "doctors", "num_jokes": 3})
+print(result)
